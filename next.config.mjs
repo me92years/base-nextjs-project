70d0c480d4850859bc0521641b70dcd8cd6fee1e
homerabbit <92077507+me92years@users.noreply.github.com>
2024-11-12 15:05:43 +0900

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+import withBundleAnalyzer from "@next/bundle-analyzer";
+
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = bundleAnalyzer({
+  eslint: {
+    dirs: ["."],
+  },
+  reactStrictMode: true,
+});
 
 export default nextConfig;
